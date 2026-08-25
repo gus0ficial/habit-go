83658e0721094fda2298cf9215586c21afcd3af4
Gustav0 <gacqoficial@gmail.com>
2026-08-24 23:33:36 -0400

    creados un archivo

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+* @Author: gustav0
+* @Date:   2026-08-24 23:31:02
+* @Last Modified by:   Gustav0
+* @Last Modified time: 2026-08-24 23:32:11
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hola Mundo")
+}
